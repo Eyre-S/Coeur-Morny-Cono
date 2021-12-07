ac6a5ef9454db360b1da5285584b17e197465efb
Eyre_S <sukazyo@outlook.com>
2021-12-07 18:08:06 +0800

    版本信息与编译信息添加时间戳记录

--- src/main/java/cc/sukazyo/cono/morny/GradleProjectConfigures.java
+++ src/main/java/cc/sukazyo/cono/morny/GradleProjectConfigures.java
@@ -4,6 +4,6 @@ package cc.sukazyo.cono.morny;
  * the final field that will be updated by gradle automatically.
  */
 public class GradleProjectConfigures {
-	public static final String VERSION = "0.3.4.2";
-	public static final long COMPILE_TIMESTAMP = 1638848971137L;
+	public static final String VERSION = "0.3.4.3";
+	public static final long COMPILE_TIMESTAMP = 1638871584208L;
 }
