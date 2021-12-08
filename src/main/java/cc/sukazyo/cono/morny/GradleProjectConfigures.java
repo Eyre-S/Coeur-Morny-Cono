4bb634c3f38470c0d2ea07d0ab3c909c29be7b94
Eyre_S <sukazyo@outlook.com>
2021-12-08 12:51:50 +0800

    修订 - 更新打包主类

--- src/main/java/cc/sukazyo/cono/morny/GradleProjectConfigures.java
+++ src/main/java/cc/sukazyo/cono/morny/GradleProjectConfigures.java
@@ -4,6 +4,6 @@ package cc.sukazyo.cono.morny;
  * the final field that will be updated by gradle automatically.
  */
 public class GradleProjectConfigures {
-	public static final String VERSION = "0.4.0.0";
-	public static final long COMPILE_TIMESTAMP = 1638938409169L;
+	public static final String VERSION = "0.4.0.1";
+	public static final long COMPILE_TIMESTAMP = 1638938903400L;
 }
