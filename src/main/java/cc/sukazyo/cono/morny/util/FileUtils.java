5046a272665d45471f9f2d27584db99fbc9010c7
Eyre_S <sukazyo@outlook.com>
2022-03-16 17:51:56 +0800

    runtime 主机名显示，信息容错报错
    - 在 runtime - system 栏中添加主机名显示
      - 如果显示出错则会是 "<unknown>"
    - runtime - morny version 改名为 coeur version
    - MornySystem.getJarMd5 出错时现在不会再直接返回错误而是会返回 "<non-jar-runtime>" 或是 "<calculation-error>" 信息

--- src/main/java/cc/sukazyo/cono/morny/util/FileUtils.java
+++ src/main/java/cc/sukazyo/cono/morny/util/FileUtils.java
@@ -10,23 +10,18 @@ import java.security.NoSuchAlgorithmException;
 public class FileUtils {
 	
 	@Nonnull
-	public static String getMD5Three (@Nonnull String path) {
+	public static String getMD5Three (@Nonnull String path) throws IOException, NoSuchAlgorithmException {
 		final BigInteger bi;
-		try {
-			final byte[] buffer = new byte[8192];
-			int len;
-			final MessageDigest md = MessageDigest.getInstance("MD5");
-			final FileInputStream fis = new FileInputStream(path);
-			while ((len = fis.read(buffer)) != -1) {
-				md.update(buffer, 0, len);
-			}
-			fis.close();
-			final byte[] b = md.digest();
-			bi = new BigInteger(1, b);
-		} catch (NoSuchAlgorithmException | IOException e) {
-			e.printStackTrace(System.out);
-			return e.getMessage();
+		final byte[] buffer = new byte[8192];
+		int len;
+		final MessageDigest md = MessageDigest.getInstance("MD5");
+		final FileInputStream fis = new FileInputStream(path);
+		while ((len = fis.read(buffer)) != -1) {
+			md.update(buffer, 0, len);
 		}
+		fis.close();
+		final byte[] b = md.digest();
+		bi = new BigInteger(1, b);
 		return bi.toString(16);
 	}
 	
