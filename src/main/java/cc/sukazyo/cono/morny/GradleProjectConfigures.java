7ce151b9bfe7302e72a3328e0b3eda9ca92f13c0
Eyre_S <sukazyo@outlook.com>
2022-05-10 23:10:38 +0800

    添加 calling"安妮今天吃什么" 和其权限控制参数
    > merged from major/0.6-hot-update/calling-last-dinner-get
    - 添加对私聊的 "安妮今天吃什么" 事件处理
    - 添加 --trusted-reader-dinner 参数用于指定一个用户可以获取"安妮今天吃什么"的数据
      - 参数可以重复指定，以便为多个账户授权

--- src/main/java/cc/sukazyo/cono/morny/GradleProjectConfigures.java
+++ src/main/java/cc/sukazyo/cono/morny/GradleProjectConfigures.java
@@ -4,6 +4,6 @@ package cc.sukazyo.cono.morny;
  * the final field that will be updated by gradle automatically.
  */
 public class GradleProjectConfigures {
-	public static final String VERSION = "0.6.3.2";
-	public static final long COMPILE_TIMESTAMP = 1651806802187L;
+	public static final String VERSION = "0.6.3.3";
+	public static final long COMPILE_TIMESTAMP = 1652195187089L;
 }
