ab37144d45d33240eceddab8d536436f4841e7dc
Eyre_S <sukazyo@outlook.com>
2022-09-19 23:54:02 +0800

    建立了 OnRandomlyTrigged 事件用于一些好玩的触发彩蛋，添加了奇怪的 CODE_IK0XA1 功能（但未启用），为新的 maven 仓库的 api changes 做了补丁
    - dependencies upgrade:
      - spotbugs 4.5.3 -> 4.7.2
      - junit 5.8.2 -> 5.9.0

--- src/main/java/cc/sukazyo/cono/morny/GradleProjectConfigures.java
+++ src/main/java/cc/sukazyo/cono/morny/GradleProjectConfigures.java
@@ -4,7 +4,7 @@ package cc.sukazyo.cono.morny;
  * the final field that will be updated by gradle automatically.
  */
 public class GradleProjectConfigures {
-	public static final String VERSION = "0.7.2.0";
+	public static final String VERSION = "0.7.2.1";
 	public static final String CODENAME = "fuzhou";
-	public static final long COMPILE_TIMESTAMP = 1655816387446L;
+	public static final long COMPILE_TIMESTAMP = 1663602816303L;
 }
