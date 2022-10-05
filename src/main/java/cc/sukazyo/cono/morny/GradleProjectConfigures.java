7bb179658e19d06ce6b41a63225145ef1a44fb97
Eyre_S <sukazyo@outlook.com>
2022-10-05 21:19:45 +0800

    添加了一个可以抽取 "debian安装镜像" 的命令 /install

--- src/main/java/cc/sukazyo/cono/morny/GradleProjectConfigures.java
+++ src/main/java/cc/sukazyo/cono/morny/GradleProjectConfigures.java
@@ -4,7 +4,7 @@ package cc.sukazyo.cono.morny;
  * the final field that will be updated by gradle automatically.
  */
 public class GradleProjectConfigures {
-	public static final String VERSION = "0.8.0.2";
+	public static final String VERSION = "0.8.0.3";
 	public static final String CODENAME = "fuzhou";
-	public static final long COMPILE_TIMESTAMP = 1664867846222L;
+	public static final long COMPILE_TIMESTAMP = 1664975839021L;
 }
--- src/main/java/cc/sukazyo/cono/morny/bot/command/MornyCommands.java
+++ src/main/java/cc/sukazyo/cono/morny/bot/command/MornyCommands.java
@@ -57,6 +57,7 @@ public class MornyCommands {
 		}
 	}
 	
+	@SuppressWarnings("NonAsciiCharacters")
 	public MornyCommands () {
 		
 		register(
@@ -85,7 +86,8 @@ public class MornyCommands {
 				new 喵呜.揉揉(),
 				new 喵呜.蹭蹭(),
 				new 喵呜.贴贴(),
-				new 私わね()
+				new 私わね(),
+				new 喵呜.Progynova()
 		);
 		
 	}
