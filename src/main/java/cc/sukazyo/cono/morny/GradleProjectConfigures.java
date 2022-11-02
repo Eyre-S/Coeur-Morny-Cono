5f8cbcffd68c86e07017dfa3a45542d16eb7c2d7
Eyre_S <sukazyo@outlook.com>
2022-11-02 16:03:35 +0800

    关闭 tracker 功能

--- src/main/java/cc/sukazyo/cono/morny/GradleProjectConfigures.java
+++ src/main/java/cc/sukazyo/cono/morny/GradleProjectConfigures.java
@@ -4,7 +4,7 @@ package cc.sukazyo.cono.morny;
  * the final field that will be updated by gradle automatically.
  */
 public class GradleProjectConfigures {
-	public static final String VERSION = "0.8.0.10";
+	public static final String VERSION = "0.8.0.11";
 	public static final String CODENAME = "putian";
-	public static final long COMPILE_TIMESTAMP = 1666096021418L;
+	public static final long COMPILE_TIMESTAMP = 1667376095614L;
 }
