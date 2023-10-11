9ec10a667412d82d8142dfa7c1cee20639016b37
Eyre_S <sukazyo@outlook.com>
2023-10-11 16:56:54 +0800

    complete some encrypt test and InputCommand test

--- src/test/scala/cc/sukazyo/cono/morny/test/utils/tgapi/InputCommandTest.scala
+++ src/test/scala/cc/sukazyo/cono/morny/test/utils/tgapi/InputCommandTest.scala
@@ -1,19 +1,70 @@
 package cc.sukazyo.cono.morny.test.utils.tgapi
 
 import cc.sukazyo.cono.morny.test.MornyTests
+import cc.sukazyo.cono.morny.util.tgapi.InputCommand
+import org.scalatest.prop.TableDrivenPropertyChecks
 
-class InputCommandTest extends MornyTests {
+class InputCommandTest extends MornyTests with TableDrivenPropertyChecks {
 	
 	"while create new InputCommand :" - {
 		
-		s"while input is $pending_val:" - {
+		val examples = Table[String|Array[String], String, String|Null, Array[String]](
+			(
+				"source",
+				"command",
+				"target",
+				"args"
+			),
+			(
+				"/exit@sukazyo_deving_bot",
+				"/exit",
+				"sukazyo_deving_bot",
+				Array.empty[String]
+			),
+			(
+				"/test@a@b",
+				"/test",
+				"a@b",
+				Array.empty[String]
+			),
+			(
+				"/test-data@random#user",
+				"/test-data",
+				"random#user",
+				Array.empty[String]
+			),
+			(
+				"/info@sukazyo_deving_bot stickers.ID_403",
+				"/info",
+				"sukazyo_deving_bot",
+				Array("stickers.ID_403")
+			),
+			(
+				"/info some extra info",
+				"/info",
+				null,
+				Array("some", "extra", "info")
+			),
+		)
+		
+		examples forEvery { (source, command, target, args) =>
 			
-			s"command should be $pending_val" in pending
-			s"target should be $pending_val" in pending
-			
-			"args array should always exists" in pending
-			
-			s"args should parsed to array $pending_val" in pending
+			val _source_describe = source match
+				case s: String => s
+				case r: Array[String] => r.mkString
+			s"while input is $_source_describe:" - {
+				
+				val _ic: InputCommand = source match
+					case s: String => InputCommand(s)
+					case r: Array[String] => InputCommand(r)
+				
+				s"command should be '$command'" in { _ic.command shouldEqual command }
+				s"target should be '$target'" in {_ic.target shouldEqual target}
+				
+				"args array should always exists" in { _ic.args shouldNot equal (null) }
+				s"args should parsed to array ${args.mkString}" in { _ic.args shouldEqual args }
+				
+			}
 			
 		}
 		
