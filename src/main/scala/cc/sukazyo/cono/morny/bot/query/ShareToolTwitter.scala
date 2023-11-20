2687c3be883234b428534b43027da2fd224454bf
Eyre_S <sukazyo@outlook.com>
2023-11-20 17:12:10 +0800

    add FixTweet, removed c.vxtwitter

--- src/main/scala/cc/sukazyo/cono/morny/bot/query/ShareToolTwitter.scala
+++ src/main/scala/cc/sukazyo/cono/morny/bot/query/ShareToolTwitter.scala
@@ -10,10 +10,10 @@ import scala.util.matching.Regex
 class ShareToolTwitter extends ITelegramQuery {
 	
 	private val TITLE_VX = "[tweet] Share as VxTwitter"
-	private val TITLE_VX_COMBINED = "[tweet] Share as VxTwitter(combination)"
 	private val ID_PREFIX_VX = "[morny/share/twitter/vxtwi]"
-	private val ID_PREFIX_VX_COMBINED = "[morny/share/twitter/vxtwi_combine]"
-	private val REGEX_TWEET_LINK: Regex = "^(?:https?://)?((?:(?:c\\.)?vx|fx|www\\.)?twitter\\.com)/((\\w+)/status/(\\d+)(?:/photo/(\\d+))?)/?(\\?[\\w&=-]+)?$"r
+	private val TITLE_FX = "[tweet] Share as Fix-Tweet"
+	private val ID_PREFIX_FX = "[morny/share/twitter/fxtwi]"
+	private val REGEX_TWEET_LINK: Regex = "^(?:https?://)?((?:(?:c\\.)?vx|fx|www\\.)?twitter|(?:www\\.|fixup)?x)\\.com/((\\w+)/status/(\\d+)(?:/photo/(\\d+))?)/?(\\?[\\w&=-]+)?$"r
 	
 	override def query (event: Update): List[InlineQueryUnit[_]] | Null = {
 		
@@ -24,12 +24,12 @@ class ShareToolTwitter extends ITelegramQuery {
 			case REGEX_TWEET_LINK(_, _path_data, _, _, _, _) =>
 				List(
 					InlineQueryUnit(InlineQueryResultArticle(
-						inlineQueryId(ID_PREFIX_VX+event.inlineQuery.query), TITLE_VX,
-						s"https://vxtwitter.com/$_path_data"
+						inlineQueryId(ID_PREFIX_FX + event.inlineQuery.query), TITLE_FX,
+						s"https://fxtwitter.com/$_path_data"
 					)),
 					InlineQueryUnit(InlineQueryResultArticle(
-						inlineQueryId(ID_PREFIX_VX_COMBINED+event.inlineQuery.query), TITLE_VX_COMBINED,
-						s"https://c.vxtwitter.com/$_path_data"
+						inlineQueryId(ID_PREFIX_VX+event.inlineQuery.query), TITLE_VX,
+						s"https://vxtwitter.com/$_path_data"
 					))
 				)
 			
