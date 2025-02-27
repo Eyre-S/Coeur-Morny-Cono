bd63567e8a895a82e723abd90bebc9a1fd67070e
Eyre_S <sukazyo@outlook.com>
2025-02-27 18:37:15 +0800

    fix test framework in MornyCoeur

--- morny-coeur/src/test/scala/cc/sukazyo/cono/morny/test/MornyTests.scala
+++ morny-coeur/src/test/scala/cc/sukazyo/cono/morny/test/MornyTests.scala
@@ -1,12 +1,8 @@
 package cc.sukazyo.cono.morny.test
 
-import cc.sukazyo.restools.{ResourceDirectory, ResourcePackage}
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should
 
-abstract class MornyTests extends AnyFreeSpec with should.Matchers {
-	
-	val pack: ResourcePackage = ResourcePackage.get("assets_morny_tests")
-	val assets: ResourceDirectory = pack.getDirectory("assets_morny_tests")
-	
-}
+abstract class MornyTests
+	extends AnyFreeSpec
+		with should.Matchers
